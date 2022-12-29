aaa7d0241a09d75a2425609cda005caae1dae6da
Sami Islam <islam.samipkr@gmail.com>
2022-12-28 19:58:19 -0500

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ input.addEventListener("change", (e) => {
   const network = new brain.NeuralNetwork()
   console.log(rgb);
   network.train([
-    { input: { r: 0.62, g: 0.72, b: 0.88 }, output: { light: 1 } },
+    { input: { r: 0.62, g: 0.72, b: 0.88 }, output: { light: 1 } },  
     { input: { r: 0.1, g: 0.84, b: 0.72 }, output: { light: 1 } },
     { input: { r: 0.33, g: 0.24, b: 0.29 }, output: { dark: 1 } },
     { input: { r: 0.74, g: 0.78, b: 0.86 }, output: { light: 1 } },
